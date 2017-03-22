d2c586f21e48ee1229408890256c037994ac1e67
naresh <naresh.sherwal@gmail.com>
2017-03-22 09:57:41 +0530

    sd: first rails new + mysql commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SdSquared::Application.config.secret_key_base = '82addbb5a5232e32120e3c9f4eba48c15ffb441c0e66c6d037c60f42db5509ad4f72659831fea6205488e9d0e9ff8a5c5b5996a68d008aa58622505e4980a793'
